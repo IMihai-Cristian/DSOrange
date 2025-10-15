dbf145355969e2db6ac006ae69144d826252de4e
Mihai Ilie <mihai.ilie@nxdata.ro>
2025-10-15 21:32:33 +0300

    memory

--- curs_03/functii.py
+++ curs_03/functii.py
@@ -31,12 +31,96 @@
 
 """ Parametrii Default(standard) """
 
-def func_2(nume='Radu', cantitate=100, device='ceasuri'):
-    result = f'{nume} a comandat {cantitate} bucati din categoria {device}'
-    return result
+# def func_2(nume='Radu', cantitate=100, device='ceasuri'):
+#     result = f'{nume} a comandat {cantitate} bucati din categoria {device}'
+#     return result
 
 # print(func_2())
 # print(func_2('Ion', 50))
 # print(func_2(device='tablete'))
 # print(func_2('Alina', device='tablete'))
-print(func_2('Alina', nume='tablete'))
+# print(func_2('Alina', nume='tablete'))
+
+# rezumat:
+# CAZ 1: parametrii pozitionali: def func(a, b, c)
+# 1. argumente pozitionale func(10, 20, 30) -> conteaza atat ordinea cat si ca nr. argumente = nr. parametrii
+# 2. argumente keyword(c=10, a=20, b=30) -> nu conteaza ordinea ci doar nr. argumente = nr. parametrii
+# 3. argumente mix(10, c=20, b=30) -> conteaza ca intotdeauna pozitional inainte si keyword dupa iar nr. argumente = nr. parametrii
+
+# CAZ 2: parametrii default: def func(a=1, b=3, c=7)
+# 1. argumente pozitionale func(10, 20, 30) -> conteaza ordinea si ca nr. argumente <= nr. parametrii
+#                                              (daca sunt mai putine argumente, dupa ultimul furnizat restul preiau
+#                                              valoarea default a parametrilor iar daca sunt mai multi-->eroare)
+# 2. argumente keyword(c=10, a=20, b=30) -> nu conteaza ordinea ci doar ca nr. argumente <= nr. parametrii
+# #                                        (daca sunt mai putine argumente, cele care lipsesc preiau
+# #                                        valoarea default a parametrilor iar daca sunt mai multi-->eroare)
+# 3. argumente mix(10, c=20, b=30) -> conteaza ca intotdeauna pozitional inainte si keyword dupa, iar nr. argumente <= nr. parametrii
+# #                                        (daca sunt mai putine argumente, cele care lipsesc preiau
+# #                                        valoarea default a parametrilor iar daca sunt mai multi-->eroare)
+
+# CAZ 3: parametrii mix(pozitional cu default): def func(a, b=3, c=7)  # se respecta ordinea cu param pozitionali primii
+# unde am param pozitional se respecta regulile de la caz 1
+# unde am param default se respecta regulile de la caz 2
+
+""" return """
+
+# def calc(x):
+#     if x < 0:
+#         return
+#     if x > 10:
+#         return
+#     print(x)
+#
+# # res = calc(-2)
+# res = calc(5)
+# print(res)
+
+""" ANOTARI """
+
+# def calcul(a: int , b: int, c: int) -> int:
+#     return a + b + c
+#
+# print(calcul('20', '10', '30'))
+
+
+""" args si kwargs """
+
+# def suma(a, b=0, *args):
+    # print(type(args))
+    # initial = a + b
+    # total = 0
+    # for elem in args:
+    #     total += elem
+    # return initial + total
+#
+# var = suma(1, 2, 3, 4, 5, 6)
+# print(var)
+# print(suma(10))
+
+
+# def suma(a, b=0, *args, **kwargs):
+#     initial = a + b
+#     print(initial)
+#     total = 0
+#     for elem in args:
+#         total += elem
+#     for key, value in kwargs.items():
+#         total += value
+#     # print(kwargs)
+#     return initial + total
+#
+# # var = suma(1, 2, 3, 4, 5, 6, c=7, bb=8, x=10)
+# var = suma(2, c=10, bb=20, x=30, d=15)
+# print(var)
+
+""" RECURSIVITATE """
+
+def test_func(nr):
+    if nr > 100:
+        return nr
+    else:
+        print(f'Nr. este acum {nr}')
+        return test_func(nr+10)
+
+val = test_func(57)
+print(val)
---
+++ curs_03/memory_savers.py
@@ -0,0 +1,99 @@
+""" Functie lambda (anonima) """
+
+# element = lambda x: x * 10
+#
+# def element_1(x):
+#     return x * 10
+
+# print(element_1)
+#
+# print(element(10))
+# print(element_1(10))
+#
+# element_2 = lambda x, y: x + y
+# print(element_2(11, 21))
+
+
+""" Filter """
+# list_1 = [1, 5, 3, 6, 8, 12, 4, 3, 11]
+#
+# # ex cu filter
+# list_2 = filter(lambda x: (x % 2 == 0), list_1)
+# print(list(list_2))
+#
+# # ex cu for:
+# list_3 = []
+# for i in list_1:
+#     if i % 2 == 0:
+#         list_3.append(i)
+# print(list_3)
+#
+# # ex functie clasica:
+#
+# def filtrare(var):
+#     if var % 2 == 0:
+#         return True
+#     else:
+#         return False
+#
+# apelare = filter(filtrare, list_1)
+# print(list(apelare))
+
+
+""" MAP """
+# list_1 = [1, 5, 3, 6, 8, 12, 4, 3, 11]
+# list_2 = map(lambda x: x * 2 if x % 2 == 0 else x * 10 , list_1)
+# print(list(list_2))
+
+""" ZIP """
+
+# list_with_int = [1, 2, 3, 4, 5, 6]
+# list_with_str = ['one', 'two', 'three', 'four', 'five', 'six']
+
+# result = zip(list_with_int, list_with_str)
+# print(list(result))
+
+# list_with_decimals = [1.1, 2.2, 3.3, 4.4, 5.5]
+
+# result_2 = zip(list_with_int, list_with_str, list_with_decimals)
+# print(list(result_2))
+
+""" UNZIP """
+
+# val_1, val_2 = zip(*result)
+# print('val_1 -> ', list(val_1))
+# print('val_2 -> ', list(val_2))
+
+
+""" COMPREHENSION LIST """
+
+var = 'comprehension'
+# caz cu for
+list_ex = []
+for elem in var:
+    list_ex.append(elem)
+print(list_ex)
+
+# caz cu map
+list_map = list(map(lambda x: x, var))
+print(list_map)
+
+# caz cu comprehension:
+list_comp = [elem for elem in var]
+print(list_comp)
+
+list_comp_2 = [elem for elem in var if elem in ['c', 'h', 'n']]
+
+list_with_int = [elem for elem in range(100) if elem % 2 == 0 if elem % 5 == 0]
+print(list_with_int)
+result_3 = ["par" if elem % 2 == 0 else "impar" for elem in range(20)]
+print(result_3)
+
+
+"""DICTIONARY COMPREHENSION"""
+
+squared_numbers = {num: num for num in range(1, 11)}
+print(squared_numbers)
+
+
+"""ANY SI ALL si ISINSTANCE """
